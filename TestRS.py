1c2ce0e4a642f71427ac11a8ead2f6d0dd192a7c
yishaia zabary <yz@sky-pick.com>
2021-03-18 18:22:17 +0200

    ru dict extracted updated function calls

--- TestRS.py
+++ TestRS.py
@@ -17,7 +17,7 @@ class TestRS(unittest.TestCase):
 
     def test_get_user_and_items_cnts(self):
         file_path = "Data/userTrainData.csv"
-        items_ids, users_ids, ranking_mean = RecommenderSystem.get_unique_users_and_items(file_path)
+        items_ids, users_ids, ranking_mean, ru_dict = RecommenderSystem.get_unique_users_and_items(file_path)
         print('ranking_mean = {}'.format(ranking_mean))
         self.assertEqual(len(items_ids), 159168)
         self.assertEqual(len(users_ids), 239007)
